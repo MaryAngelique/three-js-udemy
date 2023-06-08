c743accb0340303927f20b5717dfdb80c7953b28
Angelique Abacajan <ma_utsey@yahoo.com>
2023-06-08 13:19:32 -0400

    Implemented Cascaded Shadow Maps rendering 4 frustums with 4 directional lights with shadows and different shadow map sizes

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,125 +2,173 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 import { GUI } from 'dat.gui'
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
-import { BokehPass } from 'three/examples/jsm/postprocessing/BokehPass'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { CSM } from 'three/examples/jsm/csm/CSM'
+import { CSMHelper } from 'three/examples/jsm/csm/CSMHelper'
+
 const scene = new THREE.Scene()
-const envTexture = new THREE.CubeTextureLoader().load([
-    'img/px_25.jpg',
-    'img/nx_25.jpg',
-    'img/py_25.jpg',
-    'img/ny_25.jpg',
-    'img/pz_25.jpg',
-    'img/nz_25.jpg',
-])
-scene.background = envTexture
-const ambientLight = new THREE.AmbientLight(0x444444)
-scene.add(ambientLight)
-const light1 = new THREE.PointLight()
-light1.position.set(-6, 10, -6)
-light1.castShadow = true
-light1.shadow.mapSize.height = 1024
-light1.shadow.mapSize.width = 1024
-scene.add(light1)
+scene.background = new THREE.Color(0x87ceeb)
+
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.01,
-    100
+    0.1,
+    1000
 )
-camera.position.set(3, 1.5, 3)
+camera.position.set(4, 1, 7)
+
+const csm = new CSM({
+    fade: true,
+    far: camera.far,
+    cascades: 4,
+    shadowMapSize: 4096,
+    lightDirection: new THREE.Vector3(-1, -1, 0),
+    camera: camera,
+    parent: scene,
+    lightIntensity: 0.5
+})
+
+console.log(csm)
+
+const csmHelper = new CSMHelper(csm)
+csmHelper.displayFrustum = true
+csmHelper.displayPlanes = true
+csmHelper.displayShadowBounds = true
+scene.add(csmHelper as any)
+
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 document.body.appendChild(renderer.domElement)
-const renderPass = new RenderPass(scene, camera)
-const bokehPass = new BokehPass(scene, camera, {
-    focus: 1.0,
-    aperture: 0.0001,
-    maxblur: 1.0,
-    width: window.innerWidth,
-    height: window.innerHeight,
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.dampingFactor = 0.05
+controls.enableDamping = true
+
+const trees = ['birchTreeWithLeaves', 'saplingTree', 'tree1WithLeaves']
+const material = new THREE.MeshPhongMaterial({ color: 0x567d46 })
+
+const gLTFLoader = new GLTFLoader()
+trees.forEach((tree) => {
+    gLTFLoader.load(
+        'models/' + tree + '.glb',
+        (gltf) => {
+            let childObjectCount = 0
+            gltf.scene.traverse(function (child) {
+                if ((child as THREE.Mesh).isMesh) {
+                    child.castShadow = true
+                    childObjectCount++
+                }
+            })
+            const instancesCount = 100
+            const positions = []
+            for (let i = 0; i < instancesCount; i++) {
+                positions.push(
+                    new THREE.Vector3(
+                        Math.random() * 200 - 100,
+                        0,
+                        Math.random() * 200 - 100
+                    )
+                )
+            }
+            const scales = []
+            for (let i = 0; i < instancesCount; i++) {
+                scales.push({
+                    x: Math.random() + 2,
+                    y: Math.random() + 2,
+                    z: Math.random() + 2
+                })
+            }
+            const rotations = []
+            for (let i = 0; i < instancesCount; i++) {
+                rotations.push(Math.random() * Math.PI * 2)
+            }
+            for (let i = 0; i < childObjectCount; i++) {
+                const geometry = (
+                    gltf.scene.children[2].children[i] as THREE.Mesh
+                ).geometry
+                const copy = new THREE.InstancedMesh(
+                    geometry,
+                    (gltf.scene.children[2].children[i] as THREE.Mesh).material,
+                    instancesCount
+                )
+                copy.castShadow = true
+                const matrix = new THREE.Matrix4()
+                for (let j = 0; j < instancesCount; j++) {
+                    matrix.makeRotationY(rotations[j])
+                    matrix.makeScale(scales[j].x, scales[j].y, scales[j].z)
+                    matrix.setPosition(positions[j])
+                    copy.setMatrixAt(j, matrix)
+                }
+                scene.add(copy)
+            }
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
 })
-const composer = new EffectComposer(renderer)
-composer.addPass(renderPass)
-composer.addPass(bokehPass)
-const orbitControls = new OrbitControls(camera, renderer.domElement)
-orbitControls.enableDamping = true
-orbitControls.target.set(1, 0, 0)
-const planeGeometry = new THREE.PlaneGeometry(25, 25)
-const texture = new THREE.TextureLoader().load('img/grid.png')
-const plane = new THREE.Mesh(
-    planeGeometry,
-    new THREE.MeshPhongMaterial({ map: texture })
-)
-plane.rotateX(-Math.PI / 2)
-plane.position.y = -1
-plane.receiveShadow = true
-scene.add(plane)
+
+const planeGeometry = new THREE.PlaneGeometry(200, 200)
+const planeMesh = new THREE.Mesh(planeGeometry, material)
+planeMesh.rotateX(-Math.PI / 2)
+planeMesh.receiveShadow = true
+scene.add(planeMesh)
+
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
-    composer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
-const pivot = new THREE.Object3D()
-scene.add(pivot)
-const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(2048)
-const cubeCamera = new THREE.CubeCamera(0.1, 100, cubeRenderTarget)
-scene.add(cubeCamera)
-const material = new THREE.MeshPhysicalMaterial({
-    reflectivity: 1.0,
-    transmission: 1.0,
-    roughness: 0,
-    metalness: 0,
-    clearcoat: 0.3,
-    clearcoatRoughness: 0.25,
-    color: new THREE.Color(0xffffff),
-    ior: 1.5,
-})
-material.thickness = 50.0
 
-//cubeRenderTarget.texture.mapping = THREE.CubeRefractionMapping;
-const ball = new THREE.Mesh(new THREE.SphereGeometry(1, 32, 32), material)
-ball.position.set(1.5, 0, 0)
-ball.castShadow = true
-pivot.add(ball)
-const gui = new GUI()
-const ballFolder = gui.addFolder('Ball1')
-ballFolder.add(ball, 'visible').name('Visible')
-ballFolder.add(material, 'metalness', 0, 1.0, 0.01).name('Metalness')
-ballFolder.add(material, 'roughness', 0, 1.0, 0.01).name('Roughness')
-ballFolder.add(material, 'transmission', 0, 1.0, 0.01).name('Transmission')
-ballFolder.add(material, 'clearcoat', 0, 1.0, 0.01).name('Clearcoat')
-ballFolder
-    .add(material, 'clearcoatRoughness', 0, 1.0, 0.01)
-    .name('ClearcoatRoughness')
-ballFolder.add(material, 'reflectivity', 0, 1.0, 0.01).name('Reflectivity')
-ballFolder.add(material, 'ior', 1.0, 2.333, 0.01).name('IOR')
-ballFolder.add(material, 'thickness', 0, 50.0, 0.1).name('thickness')
-ballFolder.open()
 const stats = new Stats()
 document.body.appendChild(stats.dom)
-const clock = new THREE.Clock()
-var animate = function () {
+
+const gui = new GUI()
+const csmFolder = gui.addFolder('CSM')
+csmFolder.add(csm.lightDirection, 'x', -1, 1, 0.01)
+csmFolder.add(csm.lightDirection, 'y', -1, 1, 0.01)
+csmFolder.add(csm.lightDirection, 'z', -1, 1, 0.01)
+csmFolder.add(csm, 'lightNear', 1, 1000, 1).onChange(function (value) {
+    for (let i = 0; i < csm.lights.length; i++) {
+        csm.lights[i].shadow.camera.near = value
+        csm.lights[i].shadow.camera.updateProjectionMatrix()
+    }
+})
+csmFolder.add(csm, 'lightFar', 1, 1000, 1).onChange(function (value) {
+    for (let i = 0; i < csm.lights.length; i++) {
+        csm.lights[i].shadow.camera.far = value
+        csm.lights[i].shadow.camera.updateProjectionMatrix()
+    }
+})
+csmFolder.add(csm, 'lightIntensity', 0.1, 2, 0.1).onChange(function (value) {
+    for (let i = 0; i < csm.lights.length; i++) {
+        csm.lights[i].intensity = value
+    }
+})
+csmFolder.open()
+
+function animate() {
     requestAnimationFrame(animate)
-    orbitControls.update()
-    const ballWorldPosition = new THREE.Vector3()
-    ball.getWorldPosition(ballWorldPosition)
-    ;(bokehPass.uniforms as any).focus.value =
-        camera.position.distanceTo(ballWorldPosition)
-    const delta = clock.getDelta()
-    ball.rotateY(-0.5 * delta)
-    pivot.rotateY(0.5 * delta)
+
+    controls.update()
+
+    csm.update()
+    csmHelper.update()
+
     render()
+
     stats.update()
 }
+
 function render() {
-    cubeCamera.position.copy(camera.position)
-    cubeCamera.update(renderer, scene)
-    //renderer.render(scene, camera)
-    composer.render(0.1)
+    renderer.render(scene, camera)
 }
+
 animate()
