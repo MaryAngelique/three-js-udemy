1d8fabe09725865aa5be1e56a58f96b215853af1
Angelique Abacajan <ma_utsey@yahoo.com>
2023-06-13 15:13:00 -0400

    Implemented FFT utlizing HTML5 audio analyzer to get byte frequency data from a connected microphone

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,10 +1,12 @@
 import * as THREE from 'three'
+import { MeshBasicMaterial } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 
-const gridHelper = new THREE.GridHelper(10, 10, 0xaec6cf, 0xaec6cf)
-scene.add(gridHelper)
+const light = new THREE.AmbientLight()
+scene.add(light)
 
 const camera = new THREE.PerspectiveCamera(
     75,
@@ -12,20 +14,37 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     1000
 )
+camera.position.x = 7
+camera.position.y = 0.75
+camera.position.z = 10
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+new OrbitControls(camera, renderer.domElement)
 
-const cube = new THREE.Mesh(geometry, material)
-cube.position.set(0, 0.5, -10)
-scene.add(cube)
+const canvas = document.createElement('canvas') as HTMLCanvasElement
+canvas.width = 256
+canvas.height = 512
+
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
+
+const texture = new THREE.Texture(canvas)
+texture.minFilter = THREE.LinearFilter
+texture.magFilter = THREE.LinearFilter
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20, 256, 256),
+    new THREE.MeshPhongMaterial({
+        wireframe: true,
+        color: new THREE.Color(0x00ff00),
+        displacementMap: texture,
+        displacementScale: 10,
+    })
+)
+plane.rotateX(-Math.PI / 2)
+scene.add(plane)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -35,115 +54,73 @@ function onWindowResize() {
     render()
 }
 
-/* Liner Interpolation
- * lerp(min, max, ratio)
- * eg,
- * lerp(20, 60, .5)) = 40
- * lerp(-20, 60, .5)) = 20
- * lerp(20, 60, .75)) = 50
- * lerp(-20, -10, .1)) = -.19
- */
-function lerp(x: number, y: number, a: number): number {
-    return (1 - a) * x + a * y
+let context: AudioContext
+let analyser: AnalyserNode
+let mediaSource
+let imageData
+
+function getUserMedia(
+    dictionary: MediaStreamConstraints,
+    callback: NavigatorUserMediaSuccessCallback
+) {
+    try {
+        navigator.getUserMedia =
+            navigator.getUserMedia ||
+            (navigator as any).webkitGetUserMedia ||
+            (navigator as any).mozGetUserMedia
+        navigator.getUserMedia(dictionary, callback, (e) => {
+            console.dir(e)
+        })
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e)
+    }
 }
 
-// Used to fit the lerps to start and end at specific scrolling percentages
-function scalePercent(start: number, end: number) {
-    return (scrollPercent - start) / (end - start)
+function connectAudioAPI() {
+    try {
+        context = new AudioContext()
+        analyser = context.createAnalyser()
+        analyser.fftSize = 2048
+
+        navigator.mediaDevices
+            .getUserMedia({ audio: true, video: false })
+            .then(function (stream) {
+                mediaSource = context.createMediaStreamSource(stream)
+                mediaSource.connect(analyser)
+                animate()
+                context.resume()
+            })
+            .catch(function (err) {
+                alert(err)
+            })
+    } catch (e) {
+        alert(e)
+    }
 }
 
-const animationScripts: { start: number; end: number; func: () => void }[] = []
+function updateFFT() {
+    let timeData = new Uint8Array(analyser.frequencyBinCount)
 
-//add an animation that flashes the cube through 100 percent of scroll
-animationScripts.push({
-    start: 0,
-    end: 101,
-    func: () => {
-        let g = material.color.g
-        g -= 0.05
-        if (g <= 0) {
-            g = 1.0
-        }
-        material.color.g = g
-    },
-})
+    analyser.getByteFrequencyData(timeData)
 
-//add an animation that moves the cube through first 40 percent of scroll
-animationScripts.push({
-    start: 0,
-    end: 40,
-    func: () => {
-        camera.lookAt(cube.position)
-        camera.position.set(0, 1, 2)
-        cube.position.z = lerp(-10, 0, scalePercent(0, 40))
-        //console.log(cube.position.z)
-    },
-})
+    imageData = ctx.getImageData(0, 1, 256, 511)
+    ctx.putImageData(imageData, 0, 0, 0, 0, 256, 512)
 
-//add an animation that rotates the cube between 40-60 percent of scroll
-animationScripts.push({
-    start: 40,
-    end: 60,
-    func: () => {
-        camera.lookAt(cube.position)
-        camera.position.set(0, 1, 2)
-        cube.rotation.z = lerp(0, Math.PI, scalePercent(40, 60))
-        //console.log(cube.rotation.z)
-    },
-})
+    for (let x = 0; x < 256; x++) {
+        ctx.fillStyle = 'rgb(' + timeData[x] + ', 0, 0) '
+        ctx.fillRect(x, 510, 2, 2)
+    }
 
-//add an animation that moves the camera between 60-80 percent of scroll
-animationScripts.push({
-    start: 60,
-    end: 80,
-    func: () => {
-        camera.position.x = lerp(0, 5, scalePercent(60, 80))
-        camera.position.y = lerp(1, 5, scalePercent(60, 80))
-        camera.lookAt(cube.position)
-        //console.log(camera.position.x + " " + camera.position.y)
-    },
-})
-
-//add an animation that auto rotates the cube from 80 percent of scroll
-animationScripts.push({
-    start: 80,
-    end: 101,
-    func: () => {
-        //auto rotate
-        cube.rotation.x += 0.01
-        cube.rotation.y += 0.01
-    },
-})
-
-function playScrollAnimations() {
-    animationScripts.forEach((a) => {
-        if (scrollPercent >= a.start && scrollPercent < a.end) {
-            a.func()
-        }
-    })
-}
-
-let scrollPercent = 0
-
-document.body.onscroll = () => {
-    //calculate the current scroll progress as a percentage
-    scrollPercent =
-        ((document.documentElement.scrollTop || document.body.scrollTop) /
-            ((document.documentElement.scrollHeight ||
-                document.body.scrollHeight) -
-                document.documentElement.clientHeight)) *
-        100
-    ;(document.getElementById('scrollProgress') as HTMLDivElement).innerText =
-        'Scroll Progress : ' + scrollPercent.toFixed(2)
+    texture.needsUpdate = true
 }
 
 const stats = new Stats()
-document.body.appendChild(stats.dom)
+document.body.appendChild(stats.domElement)
 
 function animate() {
     requestAnimationFrame(animate)
 
-    playScrollAnimations()
+    updateFFT()
 
     render()
 
@@ -154,5 +131,6 @@ function render() {
     renderer.render(scene, camera)
 }
 
-window.scrollTo({ top: 0, behavior: 'smooth' })
-animate()
+window.onload = function () {
+    connectAudioAPI()
+}
