601595e2b0a24e28eda41b281e205233148c2da3
Angelique Abacajan <ma_utsey@yahoo.com>
2023-06-30 23:55:26 -0400

    Implemented CannonJS using a Cannonjs to modify THREE.Mesh positions and quaternions, creating a CANNON.Plane from a THREE.PlaneGeometry, and creating a CANNON.Trimesh from a THREE.TorusKnotGeometry

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,135 +1,46 @@
 import * as THREE from 'three'
-import StatsVR from 'statsvr'
-import { VRButton } from 'three/examples/jsm/webxr/VRButton'
-import { Font, FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
-import ButtonVR from './buttonvr'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
-const scene: THREE.Scene = new THREE.Scene()
+const scene = new THREE.Scene()
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-    50,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.set(0, 1.6, 3)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer({ antialias: true })
-renderer.setPixelRatio(window.devicePixelRatio)
+const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
-renderer.xr.enabled = true
-
 document.body.appendChild(renderer.domElement)
 
-document.body.appendChild(VRButton.createButton(renderer))
+const controls = new OrbitControls(camera, renderer.domElement)
+
+const geometry = new THREE.BoxGeometry()
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true,
+})
+
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
-
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
 }
 
-const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(25, 25, 10, 10)
-const floor: THREE.Mesh = new THREE.Mesh(
-    planeGeometry,
-    new THREE.MeshBasicMaterial({
-        color: 0x008800,
-        wireframe: true,
-    })
-)
-floor.rotateX(-Math.PI / 2)
-scene.add(floor)
+function animate() {
+    requestAnimationFrame(animate)
 
-const buttonVR = new ButtonVR(scene, camera, 500)
-buttonVR.addEventListener('pressedStart', (intersection: THREE.Intersection) => {
-    //console.log("pressedStart")
-})
-buttonVR.addEventListener('pressed', (intersection: THREE.Intersection) => {
-    //console.log("pressed")
-    statsVR.setCustom1(intersection.object.name)
-    screenText += intersection.object.name
-    regenerateGeometry()
-})
-buttonVR.addEventListener('pressedEnd', () => {
-    //console.log("pressedEnd")
-    statsVR.setCustom1('')
-})
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
 
-const keys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-let font: Font
+    controls.update()
 
-const loader = new FontLoader()
-loader.load('fonts/gentilis_regular.typeface.json', function (f:Font) {
-    keys.forEach((k, i) => {
-        font = f
-        const keyCube = new THREE.Mesh(
-            new THREE.BoxGeometry(0.66, 0.66, 0.33),
-            new THREE.MeshBasicMaterial({
-                color: 0xff0066,
-                transparent: true,
-                opacity: 0.5,
-            })
-        )
-        keyCube.name = k
-        keyCube.position.set(0, 0.75, -4)
-
-        let textGeometry = new TextGeometry(k, {
-            font: f,
-            size: 0.5,
-            height: 0.2,
-            curveSegments: 2,
-        })
-        const textMesh = new THREE.Mesh(
-            textGeometry,
-            new THREE.MeshBasicMaterial({ color: 0x00ffff })
-        )
-        textMesh.position.set(-0.18, 0.55, -4)
-
-        const keyCubePivot = new THREE.Object3D()
-        keyCubePivot.add(keyCube)
-        keyCubePivot.add(textMesh)
-        keyCubePivot.rotateY((-Math.PI / 16) * i + Math.PI / 4)
-
-        scene.add(keyCubePivot)
-
-        buttonVR.buttons.push(keyCube)
-    })
-})
-
-let screenText = ''
-const screenTextMesh = new THREE.Mesh(
-    new THREE.BufferGeometry(),
-    new THREE.MeshBasicMaterial({ color: 0x8888ff })
-)
-screenTextMesh.position.set(0, 1.5, -5)
-scene.add(screenTextMesh)
-
-function regenerateGeometry() {
-    let newGeometry = new TextGeometry(screenText, {
-        font: font,
-        size: 2,
-        height: 0.2,
-        curveSegments: 2,
-    })
-    newGeometry.center()
-    screenTextMesh.geometry.dispose()
-    screenTextMesh.geometry = newGeometry
+    render()
 }
-
-const statsVR = new StatsVR(scene, camera)
-statsVR.setX(0)
-statsVR.setY(0)
-statsVR.setZ(-2)
 
 function render() {
-    statsVR.update()
-
-    buttonVR.update(renderer)
-
     renderer.render(scene, camera)
 }
-
-renderer.setAnimationLoop(render)
+animate()
